05d8ef2cbd7d50ef8552750c6618c3504e81a048
eman19-meet <eman19@meet.mit.edu>
2017-11-20 20:51:53 +0200

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,31 @@
+import turtle
+
+#problem 1:
+print("eman")
+print("eman "*3)
+print("eman "*100)
+
+#problem 2:
+number1=10
+print(number1)
+number2=number1/2
+print(number2)
+
+#problem 3:
+my_list=[1,2,3]
+for i in range(len(my_list)):
+    print(my_list[i])
+for k in range(len(my_list)):
+    A+=my_list[k]
+print(A)
+
+#problem 4:
+turtle.penup()
+turtle.goto(100,100)
+
+for i in range(4):
+    turtle.forward(50)
+    tutle.left(90)
+
+
+turtle.mainloop()
